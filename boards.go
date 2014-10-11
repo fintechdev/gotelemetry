6e34f56194cb5a3b9c2aeaed718ae3a1a2a53d36
Matheus Felipe <mfelipe@e-xact.com>
2014-10-10 17:05:17 -0700

    Maps Board response from Telemetry to all fields into a Board struct

--- boards.go
+++ boards.go
@@ -1,11 +1,24 @@
 package gotelemetry
 
 type Board struct {
-	Id                 string `json:"id,omitempty"`
-	Name               string `json:"name,omitempty"`
-	Theme              string `json:"theme,omitempty"`
-	Display_board_name bool   `json:"display_board_name,omitempty"`
-	Aspect_ratio       string `json:"aspect_ratio,omitempty"`
+	Id                 string    `json:"id,omitempty"`
+	Name               string    `json:"name,omitempty"`
+	Theme              string    `json:"theme,omitempty"`
+	Columns            float64   `json:"columns,omitempty"`
+	Rows               float64   `json:"rows,omitempty"`
+	Size               []float64 `json:"size,omitempty"`
+	Aspect_ratio       string    `json:"aspect_ratio,omitempty"`
+	Display_board_name bool      `json:"display_board_name,omitempty"`
+	Widget_margins     float64   `json:"widget_margins,omitempty"`
+	Widget_padding     float64   `json:"widget_padding,omitempty"`
+	Font_size          string
+	Font_family        string
+	Account_id         string
+	Is_virtualized     bool
+	Created_at         string
+	Channel_ids        []string
+	Is_affiliated      bool
+	Updated_at         string
 }
 
 func (b *Board) CreateBoard(credentials Credentials) error {
@@ -18,8 +31,7 @@ func (b *Board) CreateBoard(credentials Credentials) error {
 	var responseBody interface{}
 	responseBody, err = sendJSONRequest(request)
 
-	b.Id = responseBody.(map[string]interface{})["id"].(string) //FIXME: Find a better way to copy all attrbs from the return to the b Board
-
+	b.mapToBoard(responseBody.(map[string]interface{}))
 	return err
 }
 
@@ -32,4 +44,34 @@ func (b *Board) DeleteBoard(credentials Credentials) error {
 
 	_, err = sendJSONRequest(request)
 	return err
+}
+
+func (b *Board) mapToBoard(m map[string]interface{}) {
+
+	b.Account_id = m["account_id"].(string)
+	b.Aspect_ratio = m["aspect_ratio"].(string)
+	b.Is_virtualized = m["is_virtualized"].(bool)
+
+	b.Size = make([]float64, 2)
+	b.Size[0] = m["size"].([]interface{})[0].(float64)
+	b.Size[1] = m["size"].([]interface{})[1].(float64)
+
+	b.Created_at = m["created_at"].(string)
+	b.Name = m["name"].(string)
+	b.Theme = m["theme"].(string)
+
+	b.Channel_ids = make([]string, 1)
+	b.Channel_ids[0] = m["channel_ids"].([]interface{})[0].(string)
+
+	b.Display_board_name = m["display_board_name"].(bool)
+	b.Font_size = m["font_size"].(string)
+	b.Id = m["id"].(string)
+	b.Is_affiliated = m["is_affiliated"].(bool)
+	b.Rows = m["rows"].(float64)
+	b.Updated_at = m["updated_at"].(string)
+	b.Columns = m["columns"].(float64)
+	b.Font_family = m["font_family"].(string)
+	b.Widget_margins = m["widget_margins"].(float64)
+	b.Widget_padding = m["widget_padding"].(float64)
+
 }
